77ff841ca2708b0d0300a4ba35312943834a104d
Adrien <148849224+AB-Ynov@users.noreply.github.com>
2023-11-17 15:58:16 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const db = require('./db'); // Assurez-vous que le chemin est correct
+
 const app = express();
 const port = 3000;
 
 // Middleware pour parser le corps des requêtes
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// Exemple de stockage des utilisateurs (remplacez cela par une base de données dans un environnement de production)
-const users = [];
 
 // Page d'accueil
 app.get('/', (req, res) => {
@@ -19,35 +18,24 @@ app.get('/', (req, res) => {
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
 
-    // Recherche de l'utilisateur dans la liste
-    const user = users.find(u => u.username === username);
+    // Requête préparée pour récupérer l'utilisateur
+    const query = 'SELECT * FROM users WHERE username = ?';
+    db.query(query, [username], (error, results) => {
+        if (error) {
+            console.error('Erreur lors de la requête de connexion :', error);
+            res.status(500).send('Erreur de serveur');
+            return;
+        }
 
-    // Vérification du mot de passe
-    if (user && bcrypt.compareSync(password, user.password)) {
-        res.send('Connexion réussie');
-    } else {
-        res.status(401).send('Nom d\'utilisateur ou mot de passe incorrect');
-    }
+        if (results.length > 0 && bcrypt.compareSync(password, results[0].password)) {
+            res.send('Connexion réussie');
+        } else {
+            res.status(401).send('Nom d\'utilisateur ou mot de passe incorrect');
+        }
+    });
 });
 
-// Page de création de compte
-app.post('/register', (req, res) => {
-    const { username, password } = req.body;
-
-    // Vérifier si l'utilisateur existe déjà
-    if (users.some(u => u.username === username)) {
-        res.status(400).send('Nom d\'utilisateur déjà utilisé');
-        return;
-    }
-
-    // Hachage du mot de passe
-    const hashedPassword = bcrypt.hashSync(password, 10);
-
-    // Stockage de l'utilisateur (à remplacer par une base de données)
-    users.push({ username, password: hashedPassword });
-
-    res.send('Compte créé avec succès');
-});
+// ... (autres routes)
 
 app.listen(port, () => {
     console.log(`Serveur en cours d'exécution sur http://localhost:${port}`);
