b799661c5f55cd853b92c4ce44407bf39a4f5d77
Adrien <148849224+AB-Ynov@users.noreply.github.com>
2023-11-17 13:55:19 +0000

    m

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 //document.getElementById("startButton").addEventListener("click", function() {
-//    alert("Félicitations ! Vous avez commencé votre voyage avec Young Entreprise !");
+//    alert("Félicitations ! Vous avez commencé votre voyage avec BAG !");
 //});
