8bfa38338b0c53cb0a8ee9f952c6f42d0706708d
Adrien <148849224+AB-Ynov@users.noreply.github.com>
2023-11-17 11:56:45 +0000

    sql

--- create_tables.sql
+++ create_tables.sql
@@ -1,6 +1,13 @@
 -- create_tables.sql
 
-CREATE TABLE users (
+-- Créez la base de données si elle n'existe pas
+CREATE DATABASE IF NOT EXISTS base_de_donnees;
+
+-- Utilisez la base de données
+USE base_de_donnees;
+
+-- Créez la table des utilisateurs
+CREATE TABLE IF NOT EXISTS users (
     id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(255) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL
---
+++ insert_test_data.sql
@@ -0,0 +1,7 @@
+-- Utilisez la base de données
+USE base_de_donnees;
+
+-- Exemple d'insertion de données de test
+INSERT INTO users (username, password_hash) VALUES ('utilisateur1', 'motdepasse1'), ('utilisateur2', 'motdepasse2');
+
+-- Ajoutez d'autres données selon votre modèle
