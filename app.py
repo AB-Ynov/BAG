2b5b83e474abdd10c0e35eee0d3a3e99d05cbac0
Adrien <148849224+AB-Ynov@users.noreply.github.com>
2023-11-17 13:35:11 +0000

    py

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Implémentez la gestion de l'authentification ici
+
+# Implémentez la gestion de l'arbre généalogique ici
+
+if __name__ == "__main__":
+    app.run(debug=True)
