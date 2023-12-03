293730342ad01941dbd5bd1cf93843396154b838
Adrien <148849224+AB-Ynov@users.noreply.github.com>
2023-12-03 19:45:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function () {
+// Logique pour afficher l'arbre généalogique et gérer les membres de la famille
+// Ajoutez des événements et des fonctions en fonction de vos besoins
 //document.getElementById("startButton").addEventListener("click", function() {
-//    alert("Félicitations ! Vous avez commencé votre voyage avec Young Entreprise !");
+//    alert("Félicitations ! Vous avez commencé votre voyage avec BAG !");
 //});
+});
