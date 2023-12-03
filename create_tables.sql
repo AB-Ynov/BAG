9d821da8a3e680c451975b04f886946f2abf2ce8
Adrien <148849224+AB-Ynov@users.noreply.github.com>
2023-12-03 19:32:38 +0100

    Create create_tables.sql

---
+++ create_tables.sql
@@ -0,0 +1,10 @@
+-- create_tables.sql
+-- Table pour les utilisateurs
+CREATE TABLE IF NOT EXISTS utilisateurs (
+    id INT PRIMARY KEY,
+    nom_utilisateur VARCHAR(255),
+    mot_de_passe VARCHAR(255),
+    niveau_acces INT
+);
+
+-- Autres tables pour les membres de la famille, les relations familiales, etc.
